ee663fa6a26077cbf5013000d5ba57c6d2d011bc
Bassam Khodrawi <bassam.k2@gso.schule.koeln>
2025-02-11 09:43:53 +0100

    FehlerBehebung für AnzeigeKunden Port

--- AnzeigeKunden.php
+++ AnzeigeKunden.php
@@ -1,5 +1,5 @@
 <?php 
-$c = oci_pconnect("BIKE", "BIKE", "localhost/orcl"); 
+$c = oci_pconnect("BIKE", "BIKE", "localhost/ORCL"); 
 if (!$c) { 
     $e = oci_error(); 
     trigger_error('Could not connect to database: '. $e['message'], E_USER_ERROR); 
